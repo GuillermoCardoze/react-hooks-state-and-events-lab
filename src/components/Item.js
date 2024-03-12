167f698c392b8aaf57ed10ad9252672c52668c45
Guillermo Cardoze <vital_tmv@yahoo.com>
2024-03-12 12:08:34 -0500

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Item({ name, category }) {
+
+  const [ isCart, isCartSetter ] = useState(true)
+  function cartHandler (){
+    isCartSetter(!isCart)
+
+  }
+  const buttContext = isCart ? "Add to Cart" : "Remove From Cart"
+  const cartClass = isCart ? "" : "in-cart"
+ 
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={cartHandler}className="add">{buttContext}</button>
     </li>
   );
 }
